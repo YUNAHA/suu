675e1341446ff2f031f47114e9666621bcb1a80a
YUNAHA <148053174+YUNAHA@users.noreply.github.com>
2024-09-24 17:07:07 +0900

    検索ページと追加ページの追加

--- syuukyuuyouka-front/src/App.js
+++ syuukyuuyouka-front/src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import { Box } from '@yamada-ui/react';
 import Home from './pages/Home';
+import Research from './pages/Research';
+import Addtion from './pages/Addtion';
 import './App.css';
 
 function App() {
@@ -21,6 +23,8 @@ function Main() {
       <Box mt={location.pathname === '/' ? "60px" : "0px"}>
         <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="Research" element={<Research/>} />
+          <Route path="Addtion" element={<Addtion/>} />
         </Routes>
       </Box>
     </Box>
