0dd2f00f305dad3dfc3f0c9dabbbdecfc92137aa
rei1915 <142165338+rei1915@users.noreply.github.com>
2024-10-03 03:24:21 +0900

    home_menyu
    ホームのメニューボタン

--- syuukyuuyouka-front/src/pages/Home.jsx
+++ syuukyuuyouka-front/src/pages/Home.jsx
@@ -1,28 +1,32 @@
-import React, { useEffect, useState, useRef, useCallback } from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
-import { Text, Flex, Box, Portal ,Button } from '@yamada-ui/react';
-
-
+import React, { useEffect, useState, useRef, useCallback } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { Text, Flex, Box, Portal, Button } from "@yamada-ui/react";
+import Header from "../components/Header";
+import { useWindowSize } from "@uidotdev/usehooks";
 
 const Home = () => {
   const navigate = useNavigate(); // useNavigateフックを使ってナビゲーション機能を取得
+  const { width, height } = useWindowSize(); // widthとheightをuseWindowSizeから取得
 
   // ボタンがクリックされたときに実行される関数
   const handleSearchClick = () => {
-    navigate('/Research'); // "/search" に遷移
+    navigate("/Research"); // "/search" に遷移
   };
 
   const handleAddClick = () => {
-    navigate('/Addtion'); // "/add" に遷移
+    navigate("/Addtion"); // "/add" に遷移
   };
 
   return (
     <>
-      東京電機大学
-      <Box>
-        <Button onClick={handleSearchClick}>検索</Button>
-        <Button onClick={handleAddClick}>追加</Button>
+      <Box position="fixed" top={height * 0.3} zIndex="200">
+        <Header />
       </Box>
     </>
   );
