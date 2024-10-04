65320c6ab2b783b4887e01fa4e5ac4e4c3f42c3f
rei1915 <142165338+rei1915@users.noreply.github.com>
2024-10-04 11:05:02 +0900

    おすすめの追加
    おすすめの追加

--- syuukyuuyouka-front/src/pages/Home.jsx
+++ syuukyuuyouka-front/src/pages/Home.jsx
@@ -6,7 +6,7 @@ import {
   useLocation,
 } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import { Text, Flex, Box, Portal, Button } from "@yamada-ui/react";
+import { Text, Flex, Box, Portal, Button, Image } from "@yamada-ui/react";
 import Header from "../components/Header";
 import { useWindowSize } from "@uidotdev/usehooks";
 
@@ -27,6 +27,97 @@ const Home = () => {
     <>
       <Box position="fixed" top={height * 0.3} zIndex="200">
         <Header />
+      </Box>
+      {/* おすすめの画像を横に3つ並べるセクション */}
+      <Box mt="20" p="4">
+        <Text fontSize="2xl" fontWeight="bold" mb="4">
+          あなたへのおすすめ
+        </Text>
+
+        {/* Flexを使って画像を横並びに配置 */}
+        <Flex justifyContent="space-between" gap="4" mb="8">
+          <Box w="30%">
+            <Image
+              src="https://images.dog.ceo/breeds/terrier-scottish/n02097298_4020.jpg" // 画像URLを指定
+              alt="おすすめ1"
+              w="100%"
+              h="auto"
+              borderRadius="md"
+              boxShadow="md"
+            />
+            <Text textAlign="center" mt="2">
+              おすすめ 1
+            </Text>
+          </Box>
+          <Box w="30%">
+            <Image
+              src="https://images.dog.ceo/breeds/terrier-scottish/n02097298_4020.jpg" // 画像URLを指定
+              alt="おすすめ2"
+              w="100%"
+              h="auto"
+              borderRadius="md"
+              boxShadow="md"
+            />
+            <Text textAlign="center" mt="2">
+              おすすめ 2
+            </Text>
+          </Box>
+          <Box w="30%">
+            <Image
+              src="https://images.dog.ceo/breeds/terrier-scottish/n02097298_4020.jpg" // 画像URLを指定
+              alt="おすすめ3"
+              w="100%"
+              h="auto"
+              borderRadius="md"
+              boxShadow="md"
+            />
+            <Text textAlign="center" mt="2">
+              おすすめ 3
+            </Text>
+          </Box>
+        </Flex>
+
+        <Flex justifyContent="space-between" gap="4">
+          <Box w="30%">
+            <Image
+              src="https://images.dog.ceo/breeds/terrier-scottish/n02097298_4020.jpg" // 新しい画像URLを指定
+              alt="おすすめ4"
+              w="100%"
+              h="auto"
+              borderRadius="md"
+              boxShadow="md"
+            />
+            <Text textAlign="center" mt="2">
+              おすすめ 4
+            </Text>
+          </Box>
+          <Box w="30%">
+            <Image
+              src="https://images.dog.ceo/breeds/terrier-scottish/n02097298_4020.jpg" // 新しい画像URLを指定
+              alt="おすすめ5"
+              w="100%"
+              h="auto"
+              borderRadius="md"
+              boxShadow="md"
+            />
+            <Text textAlign="center" mt="2">
+              おすすめ 5
+            </Text>
+          </Box>
+          <Box w="30%">
+            <Image
+              src="https://images.dog.ceo/breeds/terrier-scottish/n02097298_4020.jpg" // 新しい画像URLを指定
+              alt="おすすめ6"
+              w="100%"
+              h="auto"
+              borderRadius="md"
+              boxShadow="md"
+            />
+            <Text textAlign="center" mt="2">
+              おすすめ 6
+            </Text>
+          </Box>
+        </Flex>
       </Box>
     </>
   );
